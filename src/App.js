488cb1b6e490df5101710ce9028c035e08af9fb9
Simone823 <simone.daglio823@outlook.it>
2022-10-20 19:14:22 +0200

    import reviews component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+// import reviews component
+import Reviews from './components/Reviews';
+
 function App() {
   return (
     <div className="App">
-      
+      {/* reviews */}
+      <Reviews/>
     </div>
   );
 }
